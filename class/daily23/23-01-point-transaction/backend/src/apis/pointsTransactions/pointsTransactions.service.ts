5a54402ffcb9923bdcb773976d838d5c0a44b2b5
corinEEEE <cod.ing.revolu@gmail.com>
2022-08-08 20:17:14 +0900

    23일차

---
+++ class/daily23/23-01-point-transaction/backend/src/apis/pointsTransactions/pointsTransactions.service.ts
@@ -0,0 +1,49 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../users/entities/user.entity';
+import {
+  PointTransaction,
+  POINT_TRANSACTION_STATUS_ENUM,
+} from './entities/pointTranscation.entity';
+
+@Injectable()
+export class PointsTransactionsService {
+  constructor(
+    @InjectRepository(PointTransaction)
+    private readonly pointsTransactionsRepository: Repository<PointTransaction>,
+
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
+  async create({ impUid, amount, _user }) {
+    // 1.PointTransaction 테이블에 거래기록 1줄 생성
+    const pointTransaction = this.pointsTransactionsRepository.create({
+      impUid,
+      amount,
+      user: _user,
+      status: POINT_TRANSACTION_STATUS_ENUM.PAYMENT,
+    });
+
+    console.log(pointTransaction);
+
+    await this.pointsTransactionsRepository.save(pointTransaction);
+
+    console.log('11111111111');
+
+    // 2. 유저의 돈 찾아오기
+    const user = await this.usersRepository.findOne({
+      where: { id: _user.id },
+    });
+
+    // 3. 유저의 돈 업데이트
+    await this.usersRepository.update(
+      { id: _user.id },
+      { point: user.point + amount },
+    );
+
+    // 4. 최종결과 프론트엔드에 돌려주기
+    return pointTransaction;
+  }
+}
